b23c58b452a7eff3e87af463a3971258e069961a
AndersMikkelsen <ande077h@gmail.com>
2023-05-26 14:02:15 +0200

    Project update

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill } from "react-icons/bs";
